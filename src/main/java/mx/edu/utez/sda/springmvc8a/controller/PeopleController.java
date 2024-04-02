f0ddb9f704b52916d08887940bdb0f8bb2e726f3
PabloSFlores <i20213tn013@utez.edu.mx>
2024-04-01 19:29:08 -0600

    PeopleController

--- src/main/java/mx/edu/utez/sda/springmvc8a/controller/PeopleController.java
+++ src/main/java/mx/edu/utez/sda/springmvc8a/controller/PeopleController.java
@@ -4,12 +4,27 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.ArrayList;
+
 @Controller
 public class PeopleController {
+    String UNA_VARIABLE_MAL_DECLARADA = "x";
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/people/index")
     public String index() {
+        String x = "";
+        while (x != null){
+            // hola
+        }
+
         return "people-index";
     }
+//    public String index() {
+//        return "people-index";
+//    }
+//    public String index() {
+//        return "people-index";
+//    }
 }
